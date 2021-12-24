333582b806dff426178271b51e2917931900c506
Lee Mi Reuk <sslejds@naver.com>
2021-12-25 00:08:59 +0900

    Designated Initialization
    Designated Initialization (지정 초기화) 설명 및 사용법

--- C++20/C++20/C++20.cpp
+++ C++20/C++20/C++20.cpp
@@ -5,7 +5,34 @@
 #include <algorithm>
 using namespace std;
 
+// Designated Initialization (지정 초기화)
+
+struct Pos2D
+{
+    int x;
+    int y;
+};
+
+class Pos3D
+{
+public:
+    int x;
+    int y;
+    int z;
+};
+
 int main()
 {
-    std::cout << "Hello World!\n";
+    // Aggreate Initialization
+    // - 직접 만든 생성자가 없어야 함.
+    // - 가상 함수가 없어야 함.
+    // - private / protected / static인 데이터 변수는 안된다.
+    Pos3D pos{ 1,2,3 };
+
+    // ﻿Designated Initialization
+    Pos2D pos2d{ .y = 2 };
+    Pos3D pos3d{ .x = 2, .z = 3 };
+
+    // Aggreate Initialization의 경우 다른 타입을 넣어주면 축소 변환을 해주지만
+    // ﻿Designated Initialization의 경우 오류 출력
 }

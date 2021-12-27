5102a2bc5a2d776722dedf053d54db70756f2fdc
Lee Mi Reuk <sslejds@naver.com>
2021-12-27 22:14:36 +0900

    ﻿using enum in Local Scopes﻿
    ﻿using enum in Local Scopes﻿ 설명 및 사용법

--- C++20/C++20/C++20.cpp
+++ C++20/C++20/C++20.cpp
@@ -5,7 +5,56 @@
 #include <algorithm>
 using namespace std;
 
+// ﻿using enum in Local Scopes
+
+enum class Color
+{
+	Red,
+	Green,
+	Blue,
+};
+
+using enum Color;
+
 int main()
 {
-    std::cout << "Hello World!\n";
+	Color color = Color::Blue;
+
+	switch (color)
+	{
+	case Color::Red:
+		break;
+	case Color::Green:
+		break;
+	case Color::Blue:
+		break;
+	}
+	// 매번 Color:: 붙이기 싫기 때문에 using enum Color 선언
+	
+
+	switch (color)
+	{
+	case Red:
+		break;
+	case Green:
+		break;
+	case Blue:
+		break;
+	}
+	// 하지만 using enum Color를 해버리면 더이상 Color를 사용할 생각이 없음애도 불구하고
+	// Red, Green, Blue 는 Color::으로 인식한다.
+	// 전역으로 다 해주는 것이 아닌 부분으로 열어줄 수 있는 방법이 C++20 추가되었다.
+
+
+	switch (color)
+	{
+	using enum Color;
+	case Red:
+		break;
+	case Green:
+		break;
+	case Blue:
+		break;
+	}
+	/// using enum Color; 은 switch 범위를 벗어나면 유효하지 않다.
 }

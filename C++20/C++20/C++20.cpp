781b285d97205485fe2aaf504b8c09914423c126
Lee Mi Reuk <sslejds@naver.com>
2021-12-30 20:10:39 +0900

    Semaphore
    semaphore 설명 및 사용법

--- C++20/C++20/C++20.cpp
+++ C++20/C++20/C++20.cpp
@@ -5,72 +5,61 @@
 #include <algorithm>
 #include <atomic>
 #include <thread>
+#include <semaphore>
 using namespace std;
 
-// atomic
+// semaphore
+
+counting_semaphore<1> sem(0);
+vector<int> vec;
+
+void PrepareWork()
+{
+	vec.insert(vec.end(), { 1,2,3,4,5 });
+	this_thread::sleep_for(1s);
+
+	cout << "데이터 준비됨" << endl;
+	sem.release();
+}
+
+void CompleteWork()
+{
+	cout << "데이터 기다리는중..." << endl;
+	sem.acquire();
+
+	cout << "데이터 출력중... " << endl;
+
+	for (auto n : vec)
+		cout << n << endl;
+}
 
 int main()
 {
-	// atomic_ref
-	int value = 0;
+	// mutex
+	// 화장실 키
+	// [키]
+	// 상호베타적인 특성을 보장하기 위해서 사용하는게 mutex다
 
-	//int& ref = value;
-	atomic_ref<int> ref(value);
+	// semaphore
+	// 화장실
+	// [키][키][키]
+	// mutex와 같지만 최대 카운터가 1이 아니라 지정할 수 있는 n개이다.
+	
+	//counting_semaphore<3>
+	//binary_semphore = counting_semaphore<1>
 
-	// 멀티쓰레드 환경에서
-	thread t1([&ref]()
-		{
-			for (int i = 0; i < 1000000; ++i)
-				ref++;
-		}
-	);
+	//max(); // 최대 카운터 값
+	//acquire(); // counter를 1만큼 감소 (counter = 0 이면 양수가 될 때까지 대기한다.)
+	//release(update = 1); // counter를 update만큼을 증가시킨다. (대기하던 쓰레드가 있으면 풀어줌)
+	//try_acquire(); // counter가 0보다 크면 1 감소하려고 시도
+	//try_acquire_for(relTime); // 최대 relTime만큼 counter를 1감소하려고 시도
+	//try_acquire_until(absTime); // 최대 absTime까지 counter를 1감소하려고 시도
 
-	thread t2([&ref]()
-		{
-			for (int i = 0; i < 1000000; ++i)
-				ref--;
-		}
-	);
+	thread t1(PrepareWork);
+	thread t2(CompleteWork);
 
 	t1.join();
 	t2.join();
 
-
-	// 멀티쓰레드 환경이기 때문에 값이 0이 되지 않는다.
-	// atomic_ref를 사용하면 0이된다.
-	cout << ref << endl;
-
-	
-	// atomic shared_ptr
-
-	// shared_ptr (C++ 스마트 포인터 권장)
-	// [resource(data)][control block﻿(ref count)]
-	// 
-	// - control block은 Thread-Safe한게 맞다.
-	//
-
-	class User
-	{
-	public:
-		int id = 0;
-		int hp = 0;
-	};
-
-	shared_ptr<User> targetUser = make_shared<User>();
-
-
-	targetUser = make_shared<User>();
-
-	targetUser = make_shared<User>();
-
-	// 교체하는 작업은(바꿔치기하는 것은) thread-safe하지 않다.
-	// 그래서 C++20에서
-	// atomic_shared_ptr과 atomic_weak_ptr이 제안되었다.
-	// 문법 : atomic<shared_ptr<T>>, atomic<weak_ptr<T>> 
-	// shared_ptr과 weak_ptr이 atmoic하게 활용할 수 있다
-	atomic<shared_ptr<User>> atomicSharedPtr = make_shared<User>();
-	//atomicSharedPtr.compare_exchange_strong()
-
-	// LockFree 프로그래밍에서 유용
-	// 일상에서는 잘 사용x
+	return 0;
 }
